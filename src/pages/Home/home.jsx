d056a06a789941a324be2db8741ac1a153c2658c
saurabh <sourabh.impactmindz@gmail.com>
2026-06-10 16:05:36 +0530

    update

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import AnalysisPanel from "../../components/AnalysisPanel";
 import Badge from "../../components/Badge";
-import CTABanner from "../../components/Ctabanner";
+import CTABanner from "../../components/CtABanner";
 import FeatureCard from "../../components/FeatureCard";
 import Navbar from "../../components/Navbar";
 import StatCard from "../../components/StatCard";
