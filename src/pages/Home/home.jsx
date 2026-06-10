95bc9d3b9de31a156e43e6ed6260b3c79a816775
saurabh <sourabh.impactmindz@gmail.com>
2026-06-10 17:13:55 +0530

    changes

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import Badge from "../../components/Badge";
 import AnalysisPanel from "../../components/AnalysisPanel"
 import FeatureCard from "../../components/FeatureCard";
 import Navbar from "../../components/Navbar";
 import StatCard from "../../components/StatCard";
-import UploadBox from "../../components/UploadBox";
 import "../../styles/home.css";
 import CTABanner from "../../components/CTABanner";
 import { useUplodeResumeMutation } from "../../Apis/resumeApi";
 import { Settings2, Target, BarChart3, ShieldCheck } from 'lucide-react';
+import UploadBox from "../../components/UploadBox";
+import Badge from "../../components/Badge";
 
 
 const STATS = [
